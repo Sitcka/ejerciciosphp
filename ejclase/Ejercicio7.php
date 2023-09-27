782ce67ae41d7f4d752c047aa276d4c10b6d1e58
Josep Pinos <josep@gmail.com>
2023-09-27 20:13:51 +0200

    Finalizado los 7 ejercicios

--- ejclase/Ejercicio7.php
+++ ejclase/Ejercicio7.php
@@ -2,11 +2,8 @@
 // 7. Dado un número constante, por ejemplo el 169 o 193, mostrar la lista de todos sus divisores. Si
 // no los hay decir que el número es primo.
 const NUMERO = 169;
-for($i = 0; $i < NUMERO; $i++){
-    if(NUMERO % $i == 0 && NUMERO % $i == 1){
-        echo "<br>" . $i;
-    }
-    else{
-        echo "Es numero primo";
+for($i = 1; $i <= NUMERO; $i++){
+    if(NUMERO % $i == 0){
+        echo $i . "<br>";
     }
 }
