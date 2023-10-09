fbab2359fbef54285c374c8ed50a090f2cf6e6e5
Josep Pinos <josep@gmail.com>
2023-10-09 14:11:01 +0200

    Hasta el ejercicio 4 de Ejercicios avanzados

---
+++ ejclase/Ejercicios Avanzados PHP/Ejercicio4.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_GET["ienviar"]) && !empty($_GET["ienviar"])) {
+    if (isset($_GET["ilista"]) && !empty($_GET["ilista"])) {
+        $programacion = $_GET["ilista"];
+        echo "<h4>Los lenguajes de programacion de preferencia son: </h4>";
+        echo "<ol>";
+        foreach($programacion as $lenguaje){
+            echo "<li>$lenguaje</li>";
+        }
+        echo "</ol>";
+    }
+}
