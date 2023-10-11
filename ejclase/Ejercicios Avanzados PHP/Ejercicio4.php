222d734a8073e3db6f28864dc72f9cafe24688ee
Josep Pinos <josep@gmail.com>
2023-10-11 18:29:45 +0200

    Ejercicio 6 renovado

--- ejclase/Ejercicios Avanzados PHP/Ejercicio4.php
+++ ejclase/Ejercicios Avanzados PHP/Ejercicio4.php
@@ -3,5 +3,8 @@ if(isset($_GET["ienviar"]) && !empty($_GET["ienviar"])){
     if(isset($_GET["icomidas"]) && !empty($_GET["icomidas"])){
         $comida = $_GET["icomidas"];
         echo "<h4>Tu comida favorita o la que seleccionaste fue: $comida</h4>";
+    } else {
+        //Caragr dirctamente la pagina sin hacer un submit (Etiqueta input)
+        echo "<p>Formulario no seleccionado</p>";
     }
 }
